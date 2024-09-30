ef2ab8bf04ffc6694e90e40dafde7701e317f9e7
agustinusklimio <agustinus@klimio.com>
2024-09-30 12:58:41 +0200

    Add optimization action enum

---
+++ src/Common/Lib/Enums/Asset/DeviceOptimization/OptimizationAction.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Elva\Common\Lib\Enums\Asset\DeviceOptimization;
+
+enum OptimizationAction: string
+{
+    case ADJUST_INDOOR_TEMPERATURE = 'ADJUST_INDOOR_TEMPERATURE';
+    case ADJUST_TEMPERATURE_OFFSET = 'ADJUST_TEMPERATURE_OFFSET';
+
+}
