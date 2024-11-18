8efb8ded43aa0de582ee09924bc8328c169d7087
agustinusklimio <agustinus@klimio.com>
2024-11-18 11:28:16 +0100

    Add new enum for compressor and eheater operation status

---
+++ src/Common/Lib/Enums/Asset/NormalizedDeviceData/OperationStatus/CompressorOperationStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Elva\Common\Lib\Enums\Asset\NormalizedDeviceData\OperationStatus;
+
+enum CompressorOperationStatus: string
+{
+    case OFFLINE = 'OFFLINE';
+    case HEATING_CENTRAL = 'HEATING_CENTRAL';
+    case HEATING_DOMESTIC_HOT_WATER = 'HEATING_DOMESTIC_HOT_WATER';
+    case COOLING = 'COOLING';
+    case DEFROST = 'DEFROST';
+}
---
+++ src/Common/Lib/Enums/Asset/NormalizedDeviceData/OperationStatus/EHeaterOperationStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Elva\Common\Lib\Enums\Asset\NormalizedDeviceData\OperationStatus;
+
+enum EHeaterOperationStatus: string
+{
+    case OFFLINE = 'OFFLINE';
+    case HEATING_CENTRAL = 'HEATING_CENTRAL';
+    case HEATING_DOMESTIC_HOT_WATER = 'HEATING_DOMESTIC_HOT_WATER';
+    case COOLING = 'COOLING';
+}
