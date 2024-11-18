bd610794aee86acfba1eaf7f5c4efb09ead98d01
agustinusklimio <agustinus@klimio.com>
2024-11-18 16:21:36 +0100

    Fix mismatch type

--- src/Common/Lib/Data/NormalizedDeviceData.php
+++ src/Common/Lib/Data/NormalizedDeviceData.php
@@ -24,14 +24,14 @@ class NormalizedDeviceData
 
     public Carbon $timestamp;
     public string $name;
-    public object|string|float $value;
+    public object|string|float|int $value;
     public DataType $type;
     public DataUnit|null $unit;
     public array $attributes;
 
     public function __construct(Carbon $timestamp,
                                 string $name,
-                                string|float|int $value,
+                                object|string|float|int $value,
                                 DataType $type,
                                 DataUnit $unit = null,
                                 array $attributes = [])
