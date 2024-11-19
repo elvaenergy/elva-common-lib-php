3b97962819136784c36caafd279805897baa183e
agustinusklimio <agustinus@klimio.com>
2024-11-19 12:04:55 +0100

    Added supported manufacturer

---
+++ src/Common/Lib/Enums/Asset/Device/SupportedManufacturer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Elva\Common\Lib\Enums\Asset\Device;
+
+enum SupportedManufacturer: string
+{
+    case BOSCH = 'Bosch';
+    case IVT = 'IVT';
+    case THERMIA = 'Thermia';
+    case NIBE = 'Nibe';
+    case CTC = 'CTC';
+    case QVANTUM = 'Qvantum';
+    case VIESSMANN = 'Viessmann';
+    case MITSUBISHI = 'Mitsubishi';
+    case DAIKIN = 'Daikin';
+    case PANASONIC = 'Panasonic';
+    case HITACHI = 'Hitachi';
+    case TOSHIBA = 'Toshiba';
+    case SAMSUNG = 'Samsung';
+    case LG = 'LG';
+    case FUJITSU = 'Fujitsu';
+
+}
