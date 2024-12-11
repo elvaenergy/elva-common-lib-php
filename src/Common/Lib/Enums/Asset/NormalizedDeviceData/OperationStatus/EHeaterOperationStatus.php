583a29d21c6528de8c3c2c61c163c7fef9ff699f
agustinusklimio <agustinus@klimio.com>
2024-12-11 01:23:36 +0100

    Add new enum DEFROST

--- src/Common/Lib/Enums/Asset/NormalizedDeviceData/OperationStatus/EHeaterOperationStatus.php
+++ src/Common/Lib/Enums/Asset/NormalizedDeviceData/OperationStatus/EHeaterOperationStatus.php
@@ -8,4 +8,5 @@ enum EHeaterOperationStatus: string
     case HEATING_CENTRAL = 'HEATING_CENTRAL';
     case HEATING_DOMESTIC_HOT_WATER = 'HEATING_DOMESTIC_HOT_WATER';
     case COOLING = 'COOLING';
+    case DEFROST = 'DEFROST';
 }
